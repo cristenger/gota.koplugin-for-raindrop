a2352a08a3422427ea7bb9ce527bebb752287040
Christian Stenger <cristianstenger@gmail.com>
2025-07-09 19:09:05 -0400

    orden de versiones de plugin
    solamente gota plugin

--- _meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-
-return {
-  name        = "raindrop",                                  
-  fullname    = _("Raindrop.io"),                            
-  description = _("Lee y gestiona tus marcadores de Raindrop.io desde KOReader!")
-}
--- gota.koplugin.backup/_meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-
-return {
-  name        = "gota",                                  
-  fullname    = _("Gota"),                            
-  description = _("Con Gota lee y gestiona tus marcadores de Raindrop.io desde KOReader!")
-}
